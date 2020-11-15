b49ee6b97203b616a64f030b98574d8043a941ea
Hemingway <ghostblac@protonmail.com>
2020-11-15 15:35:11 -0500

    Update puppet_master_2501.py

--- puppet_master_2501.py
+++ puppet_master_2501.py
@@ -1,105 +1,110 @@
 #!/usr/bin/python
-
-# Importing os module
+​
 import argparse
 import os
-import sys
-
-
-def command_control(args):
-    """
-    """
-
-    try:
-        if args[1] == "create_file":
-            create_file(args[2], args[3])
-
-        elif args[1] == "delete_file":
-            delete_file(args[2])
-
-        elif args[1] == "create_directory":
-            create_directory(args[2])
-
-        elif args[1] == "delete_directory":
-            delete_directory(args[2])
-
-        else:
-            print_usage()
-
-    except IndexError:
-        print("Nice try, jackass.")
-        print_usage()
-
-
+​
+​
 def create_file(filename, content):
     """
     """
-
+​
     if not os.path.exists(filename):
         f = open(filename, "w")
         f.write(content)
         f.close()
         print("{0} has been created.".format(filename))
+​
     else:
-        print("File already exists.")
-
-
-def create_directory(foldername):
+        print("Oh, how original?")
+        print("{0} already exists.".format(filename))
+​
+​
+def create_dir(dirname):
     """
     """
-
-    if os.path.isdir(foldername):
-        print("This directory already exists.")
+​
+    if not os.path.isdir(dirname):
+        path = os.path.join(os.getcwd(), dirname)
+        os.mkdir(path)
+        print("{0} has been created.".format(dirname))
+​
     else:
-        print("I'll see what I can do.")
-        try:
-            path = os.path.join(os.getcwd(), foldername)
-            os.mkdir(path)
-            print("New directory created.")
-        except FileExistsError:
-            print("Bro, what is you doing?")
-
-
-def delete_directory(foldername):
+        print("Bro, what is you doin'?")
+        print("{0} already exists.".format(dirname))
+​
+​
+def delete_dir(dirname):
     """
     """
-
-    if os.path.exists(foldername):
-        os.remove(foldername)
+​
+    if os.path.exists(dirname):
+        os.rmdir(dirname)
+        print("{0} has been deleted.".format(dirname))
+​
     else:
-        print("This folder does not exist.")
-
-
+        print("{0} does not exist.".format(dirname))
+​
+​
 def delete_file(filename):
     """
     """
-
+​
     if os.path.exists(filename):
         os.remove(filename)
+        print("{0} has been deleted.".format(filename))
+​
     else:
-        print("The file does not exist.")
-
-
-def print_usage():
-    """
-    """
-
-    print("Usage: python3 puppet_master.py help")
-    print("Usage: python3 puppet_master.py create_directory [foldername]")
-    print("Usage: python3 puppet_master.py create_file [filename] [content]")
-    print("Usage: python3 puppet_master.py delete_directory [foldername]")
-    print("Usage: python3 puppet_master.py delete_file [filename]")
-
-
+        print("{0} does not exists.".format(filename))
+​
+​
 def main():
     """
     """
-
-    command_control(sys.argv)
-
-
+​
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--create_dir",
+                        action="store",
+                        nargs=1,
+                        help="Usage: python3 puppet_master.py --create_dir [dirname]",
+                        dest="create_dir_args")
+    parser.add_argument("--create_file",
+                        action="store",
+                        nargs=2,
+                        help="Usage: python3 puppet_master.py --create_file [filename] [content]",
+                        dest="create_file_args")
+    parser.add_argument("--delete_dir",
+                        action="store",
+                        nargs=1,
+                        help="Usage: python3 puppet_master.py --delete_dir [dirname]",
+                        dest="delete_dir_args")
+    parser.add_argument("--delete_file",
+                        action="store",
+                        nargs=1,
+                        help="Usage: python3 puppet_master.py --delete_file [filename]",
+                        dest="delete_file_args")
+​
+    args = parser.parse_args()
+​
+    if args.create_dir_args:
+        dirname = args.create_dir_args[0]
+        create_dir(dirname)
+​
+    elif args.create_file_args:
+        filename = args.create_file_args[0]
+        content = args.create_file_args[1]
+        create_file(filename, content)
+​
+    elif args.delete_dir_args:
+        dirname = args.delete_dir_args[0]
+        delete_dir(dirname)
+​
+    elif args.delete_file_args:
+        filename = args.delete_file_args[0]
+        delete_file(filename)
+​
+​
 if __name__ == "__main__":
     """
     """
-
+​
     main()
