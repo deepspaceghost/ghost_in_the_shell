7b4ab6e4928766295dda87e400e33cc6c8259292
Hemingway <ghostblac@protonmail.com>
2020-12-15 19:55:48 -0500

    Update test.py
    Added additional testing capabilities.

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import unittest
 import os
 from puppet_master import puppet_master
+import unittest
 
 
 class TestPuppetMaster(unittest.TestCase):
@@ -13,9 +13,38 @@ class TestPuppetMaster(unittest.TestCase):
         """
         """
 
-        puppet_master.create_file("text.txt", "test text")
-        self.assertTrue(os.path.exists("text.txt"))
-        os.remove("text.txt")
+        print("Testing function...")
+        puppet_master.create_file("test.txt", "test text")
+        self.assertTrue(os.path.exists("test.txt"))
+        os.remove("test.txt")
+
+    def test_create_dir(self):
+        """
+        """
+
+        print("Testing function...")
+        puppet_master.create_dir("test")
+        self.assertTrue(os.path.exists("test"))
+        os.rmdir("test")
+
+    def test_delete_file(self):
+        """
+        """
+
+        print("Testing function...")
+        puppet_master.create_file("test.txt", "test text")
+        puppet_master.delete_file("test.txt")
+        self.assertFalse(os.path.exists("test.txt"))
+
+    def test_rename_file(self):
+        """
+        """
+
+        print("Testing function...")
+        puppet_master.create_file("test.txt", "test text")
+        puppet_master.rename_file("test.txt", "test2.txt")
+        self.assertTrue(os.path.exists("test2.txt"))
+        os.remove("test2.txt")
 
 
 if __name__ == "__main__":
